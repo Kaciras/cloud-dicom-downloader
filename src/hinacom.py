2b28f99773248d69714904ef01527d644dc97463
Kaciras <Kaciras@protonmail.com>
2024-10-17 16:21:14 +0800

    add files

---
+++ src/hinacom.py
@@ -0,0 +1,236 @@
+import json
+import re
+import sys
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import aiohttp
+from pydicom.datadict import DicomDictionary
+from pydicom.dataset import Dataset, FileMetaDataset
+from pydicom.tag import Tag
+from pydicom.uid import ExplicitVRLittleEndian
+from pydicom.valuerep import VR, STR_VR, INT_VR, FLOAT_VR
+from tqdm import tqdm
+
+# 报告分享的 URL
+URL = "https://xxx.medicalimagecloud.com/t/xxx...xxx"
+# 分享密码
+PASSWORD = "xxxxxx"
+# 保存位置
+SAVE_DIR = Path("dicom")
+# 保存中间输出，仅调试用
+TEMP_DIR = Path("dicom_temp")
+
+# ============================================================
+
+_HEADERS = {
+	"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	"Accept-Language": "zh,zh-CN;q=0.7,en;q=0.3",
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0",
+}
+
+_LINK_VIEW = re.compile(r"/Study/ViewImage\?studyId=([\w-]+)")
+_LINK_ENTRY = re.compile(r"window\.location\.href = '([^']+)'")
+_LINK_REx = re.compile(r'var TARGET_PATH = "([^"]+)"')
+_VAR_RE = re.compile(r'var (STUDY_ID|ACCESSION_NUMBER|STUDY_EXAM_UID|LOAD_IMAGE_CACHE_KEY) = "([^"]+)"')
+
+_REFRESH_CAC = timedelta(minutes=1)
+
+
+async def get_viewer_url():
+	print(f"下载海纳医信 DICOM，报告 ID：{URL.split('/')[-1]}，密码：{PASSWORD}")
+	_headers = {"content-type": "application/x-www-form-urlencoded"}
+
+	async with aiohttp.ClientSession(headers=_HEADERS, raise_for_status=True) as client:
+		# 先是入口页面，它会重定向到登录页并设置一个 Cookie
+		async with client.get(URL) as response:
+			url = response.real_url
+			origin = str(url.origin())
+			uuid = url.path.split("/")[-1]
+
+		# 登录报告页，成功后又会拿到 Cookies，从中找查看影像的链接。
+		async with client.post(url, data=f"id={uuid}&Password={PASSWORD}", headers=_headers) as response:
+			html = await response.text()
+			return origin + _LINK_VIEW.search(html).group(0)
+
+
+async def download():
+	url = await get_viewer_url()
+
+	async with aiohttp.ClientSession(headers=_HEADERS, raise_for_status=True) as client:
+		async with client.get(url) as response:
+			html2 = await response.text()
+			Path("temp.html").write_text(html2)
+			matches = _LINK_ENTRY.search(html2)
+
+		async with client.get(matches.group(1)) as response:
+			html3 = await response.text("utf-8")
+			client._base_url = response.real_url.origin()
+			matches = _LINK_REx.search(html3)
+
+		# 查看器页，关键信息就写在 JS 里。
+		async with client.get(matches.group(1)) as response:
+			html4 = await response.text()
+			matches = _VAR_RE.findall(html4)
+			top_study_id = matches[0][1]
+			accession_number = matches[1][1]
+			exam_uid = matches[2][1]
+			ck = matches[3][1]
+
+		login_time = datetime.now()
+		params = {
+			"studyId": top_study_id,
+			"accessionNumber": accession_number,
+			"examuid": exam_uid,
+			"minThickness": "5"
+		}
+		async with client.get("/ImageViewer/GetImageSet", params=params) as response:
+			image_set = await response.json()
+			print(f'姓名：{image_set["patientName"]}')
+			print(f'检查：{image_set["studyDescription"]}')
+			print(f'日期：{image_set["studyDate"]}\n')
+
+			file = Path(f"dicom/DataSet.json")
+			file.parent.mkdir(parents=True, exist_ok=True)
+			file.write_text(json.dumps(image_set))
+
+		for series in image_set["displaySets"]:
+			name, images = series["description"].rstrip(), series["images"]
+			dir_ = Path(f"dicom/{name}")
+			dir_.mkdir()
+
+			params = {
+				"studyId": images[0]["studyId"],
+				"imageId": images[0]["imageId"],
+				"frame": "0",
+				"storageNodes": "",
+			}
+			async with client.get("/ImageViewer/GetImageDicomTags", params=params) as response:
+				tags = await response.read()
+				if tags == "[]":
+					continue
+				(dir_ / "tags.json").write_bytes(tags)
+
+			for i, info in enumerate(tqdm(images, desc=name, unit="张", file=sys.stdout)):
+				url = f"/imageservice/api/image/dicom/{info['studyId']}/{info['imageId']}/0/0"
+				async with client.get(url, params={"ck": ck}) as response:
+					metadata = response.headers["X-ImageFrame"]
+					pixels = await response.read()
+
+					(dir_ / f"{i}.json").write_text(metadata)
+					(dir_ / f"{i}.slice").write_bytes(pixels)
+
+				# 每一分钟要刷新一下 CAC_AUTH 令牌
+				if datetime.now() - login_time >= _REFRESH_CAC:
+					(await client.get("/ImageViewer/renewcacauth")).close()
+
+
+def _maybe_mv(value: str, cast_fn):
+	parts = value.split("\\")
+	if len(parts) == 1:
+		return cast_fn(value)
+	return [cast_fn(p) for p in parts]
+
+
+def _cast_value_type(value: str, vr: str):
+	if vr == VR.AT:
+		return Tag(value)
+	if vr in STR_VR:
+		return _maybe_mv(value, str)
+	if vr in INT_VR or vr == "US or SS":
+		return _maybe_mv(value, int)
+	if vr in FLOAT_VR:
+		return _maybe_mv(value, float)
+
+	raise NotImplementedError("Unsupported VR")
+
+
+def _write_dicom(metadata, pixels, tags, file):
+	ds = Dataset()
+
+	for item in tags:
+		group, element = item["tag"].split(",")
+		id_ = (int(group, 16) << 16) | int(element, 16)
+		definition = DicomDictionary.get(id_)
+		if definition:
+			vr, key = definition[0], definition[4]
+			value = _cast_value_type(item["value"], vr)
+			setattr(ds, key, value)
+		else:
+			pass
+
+	# for item in private_items:
+	# 	group, element = item["tag"].split(",")
+	# 	group, element = int(group, 16), int(element, 16)
+	# 	ds.add_new_private(private_creator, group, element, item["value"], "UN")
+
+	ds.PixelRepresentation = 1 if metadata["signed"] else 0
+	ds.BitsAllocated = metadata["bitsAllocated"]
+	ds.BitsStored = metadata["bitsStored"]
+	ds.HighBit = ds.BitsStored - 1
+	ds.Rows = metadata["rows"]
+	ds.Columns = metadata["columns"]
+	ds.SamplesPerPixel = metadata["samplesPerPixel"]
+	ds.PhotometricInterpretation = metadata["photometricInterpretation"]
+	ds.RescaleIntercept = metadata["intercept"]
+	ds.RescaleSlope = metadata["slope"]
+	ds.WindowWidth = metadata["windowWidth"]
+	ds.WindowCenter = metadata["windowCenter"]
+	ds.InstanceNumber = metadata["instanceNumber"]
+	ds.FrameOfReferenceUID = metadata["frameOfReferenceUID"]
+	ds.Modality = metadata["modality"]
+	ds.SliceThickness = metadata["sliceThickness"]
+	ds.EchoNumbers = metadata["echoNumbers"]
+	ds.EchoTrainLength = metadata["echoTrainLength"]
+	ds.RepetitionTime = metadata["timeToRepetition"]
+	ds.EchoTime = metadata["timeToEcho"]
+	ds.KVP = metadata["kvp"]
+	ds.ExposureTime = metadata["exposureTime"]
+	ds.Exposure = metadata["exposure"]
+	ds.SliceLocation = metadata["sliceLocation"]
+	ds.ConvolutionKernel = metadata["convolutionKernel"]
+	ds.SmallestImagePixelValue = metadata["minPixelValue"]
+	ds.LargestImagePixelValue = metadata["maxPixelValue"]
+	ds.ImagePosition = metadata["imagePosition"]
+
+	if ds.PixelRepresentation == 0:
+		ds.PixelPaddingValue = max(0, metadata["pixelPaddingValue"])
+	else:
+		ds.PixelPaddingValue = metadata["pixelPaddingValue"]
+
+	ds.PixelData = pixels
+
+	ds.file_meta = FileMetaDataset()
+	ds.file_meta.MediaStorageSOPClassUID = ds.SOPClassUID
+	ds.file_meta.MediaStorageSOPInstanceUID = ds.SOPInstanceUID
+	ds.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
+	ds.save_as(file, enforce_file_format=True)
+
+
+def build_dicom_files():
+	with TEMP_DIR.joinpath("DataSet.json").open() as fp:
+		studies = json.load(fp)
+		name_map = {s["description"].rstrip(): s for s in studies["displaySets"]}
+
+	for series_dir in TEMP_DIR.iterdir():
+		if series_dir.is_file():
+			continue
+		info = name_map[series_dir.name]
+		out_dir = SAVE_DIR / series_dir.name
+		out_dir.mkdir(parents=True, exist_ok=True)
+
+		tags = json.loads(series_dir.joinpath("tags.json").read_text("utf8"))
+		for i in range(len(info["images"])):
+			pixels = series_dir.joinpath(f"{i}.slice").read_bytes()
+			meta = json.loads(series_dir.joinpath(f"{i}.json").read_text())
+			_write_dicom(meta, pixels, tags, out_dir / f"{i}.dcm")
+
+
+async def main():
+	await download()
+	build_dicom_files()
+
+
+if __name__ == '__main__':
+	# asyncio.run(main())
+	build_dicom_files()
