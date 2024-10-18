a005ffc6621a9f47e6a70e47a83dc4435e5d38ef
Kaciras <Kaciras@protonmail.com>
2024-10-18 18:32:19 +0800

    remove an unnecessary finish check

--- src/szjudianyun.js
+++ src/szjudianyun.js
@@ -20,9 +20,8 @@ const tag = "0x00100010b1u2d3d4h5a0x00101001b1u2d3d4h5a0x00100020b1u2d3d4h5a0x00
 // 什么傻逼 qinniao，不会是北大青鸟吧？
 const response = await fetch("http://qinniaofu.coolingesaving.com:63001/socket.io/?EIO=3&transport=polling&t=ooooooo");
 if (!response.ok) {
-	throw new Error(response.status);
+	throw new Error(`Get token failed (${response.status})`);
 }
-
 let text = await response.text();
 text = text.slice(text.indexOf("{"), text.lastIndexOf("}") + 1);
 const { sid } = JSON.parse(text);
@@ -63,15 +62,18 @@ function requestDICOM(id, index) {
 
 await once(ws, "open");
 const { series, series_dicom_number } = await getInfo();
-let sIndex = 0;
-let dIndex = 1;
+
+let seriesIndex = 0;
+let imageIndex = 1;
+
+requestDICOM(series[seriesIndex], imageIndex);
 
 ws.addEventListener("message", async ({ data }) => {
 	if (typeof data === "string") {
-		return;
+		return; // 451 开头的回复消息，没什么用。
 	}
-	const id = series[sIndex];
-	const file = join(outDir, id, dIndex + ".dicom");
+	const id = series[seriesIndex];
+	const file = join(outDir, id, imageIndex + ".dicom");
 
 	console.log("Download: " + file);
 	mkdirSync(dirname(file), { recursive: true });
@@ -79,21 +81,17 @@ ws.addEventListener("message", async ({ data }) => {
 	// 第一位 4 是 socket.io 添加的。
 	writeFileSync(file, await data.slice(1).bytes());
 
-	if (dIndex >= series_dicom_number[id]) {
-		dIndex = 0;
-		sIndex++;
+	if (imageIndex >= series_dicom_number[id]) {
+		imageIndex = 0;
+		seriesIndex++;
 
-		if (sIndex >= series.length) {
-			return ws.close();
+		if (series[seriesIndex].startsWith("dfyfilm")) {
+			seriesIndex++;
 		}
-		if (series[sIndex].startsWith("dfyfilm")) {
-			sIndex += 1;
-		}
-		if (sIndex >= series.length) {
+		if (seriesIndex >= series.length) {
 			return ws.close();
 		}
 	}
-	requestDICOM(series[sIndex], ++dIndex);
+	requestDICOM(series[seriesIndex], ++imageIndex);
 });
 
-requestDICOM(series[sIndex], dIndex);
