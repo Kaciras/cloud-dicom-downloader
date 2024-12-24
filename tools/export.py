5da629ba08521b9a5141bd422bb9fe60ac7c6796
Kaciras <Kaciras@protonmail.com>
2024-12-24 17:25:14 +0800

    tweak video FPS

--- tools/export.py
+++ tools/export.py
@@ -2,42 +2,58 @@
 DCM 文件转换工具，支持导出为图片或视频。
 TODO: 匿名化，反向转换
 """
-from moviepy import ImageClip, concatenate_videoclips
+from moviepy import ImageClip, VideoFileClip, concatenate_videoclips
 import os
-import re
 from pathlib import Path
 import numpy as np
 from pydicom import dcmread, pixels
+from PIL import Image
 
 os.environ["FFMPEG_BINARY"] = r"D:\Program Files\ffmpeg\bin\ffmpeg.exe"
 
-def images_to_video(images: list[np.array]):
-    clips = [ImageClip(x, duration=0.1) for x in images]
-    video = concatenate_videoclips(clips, method="compose")
-    video.write_videofile("test.mp4",  fps=30)
-    print("Video created successfully:", "test.avi")
+OUTPUT_DIR = Path("exports")
+FPS = 10
+
+def images_to_video(images: list[np.array], out_file: Path):
+	clips = [ImageClip(x, duration=1 / FPS) for x in images]
+	video = concatenate_videoclips(clips, method="compose")
+
+	if out_file.suffix != ".avi":
+		video.write_videofile(out_file, fps=FPS)
+	else:
+		video.write_videofile(out_file, codec="png", fps=FPS)
+
+	print("Video created successfully:", out_file)
+
+
+def video_to_images(file: Path):
+	video = VideoFileClip(file)
+
 
 def load_series_images(directory: Path):
-    files = os.listdir(directory)
-    images = [None] * len(files)
-    
+	files = os.listdir(directory)
+	images = [None] * len(files)
+	
 	# https://github.com/ykuo2/dicom2jpg/blob/main/dicom2jpg/utils.py
-    for file in files:
-        ds = dcmread(image_dir / file)
-        i, px = ds.InstanceNumber, ds.pixel_array
-        
-        px = pixels.apply_modality_lut(px, ds)
-        px = pixels.apply_voi_lut(px, ds)
-        px = pixels.apply_presentation_lut(px, ds)
-        
-        px = ((px-px.min())/(px.max()-px.min())) * 255.0
-        
-        images[i - 1] = px.astype(np.uint8)
+	for file in files:
+		ds = dcmread(image_dir / file)
+		i, px = ds.InstanceNumber, ds.pixel_array
+		
+		px = pixels.apply_modality_lut(px, ds)
+		px = pixels.apply_voi_lut(px, ds)
+		px = pixels.apply_presentation_lut(px, ds)
+		
+		px = ((px-px.min())/(px.max()-px.min())) * 255
+		
+		images[i - 1] = px.astype(np.uint8)
 
-    return images
-    
+	return images
+
+
+def convert_all():pass
+
 if __name__ == "__main__":
-    image_dir = Path(r"download\test")
-    
-    images = load_series_images(image_dir)
-    images_to_video(images)
+	image_dir = Path(r"download\test")
+	
+	images = load_series_images(image_dir)
+	images_to_video(images, image_dir.with_suffix(".avi"))
