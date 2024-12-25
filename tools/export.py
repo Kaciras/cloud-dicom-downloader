c1ad5302d9a6037b5c166241f26e6e3dc9394b88
Kaciras <Kaciras@protonmail.com>
2024-12-25 10:45:09 +0800

    chore

--- tools/export.py
+++ tools/export.py
@@ -1,22 +1,49 @@
 """
-DCM 文件转换工具，支持导出为图片或视频。
-TODO: 匿名化
+DCM 文件转换工具，支持 DCM 文件、图片和视频之间的转换。
 """
-from moviepy import ImageClip, VideoFileClip, concatenate_videoclips
 import os
-from pathlib import Path
-import numpy as np
-from pydicom import dcmread, pixels, Dataset
-from PIL import Image
-from pydicom.uid import ExplicitVRLittleEndian, SecondaryCaptureImageStorage, PYDICOM_ROOT_UID, UID
 from hashlib import sha256
+from pathlib import Path
 
+import numpy as np
+from PIL import Image
+from moviepy import ImageClip, VideoFileClip, concatenate_videoclips
+from pydicom import dcmread, pixels, Dataset
+from pydicom.uid import ExplicitVRLittleEndian, SecondaryCaptureImageStorage, PYDICOM_ROOT_UID
+
+# moviepy 里头调用 ffmpeg 命令行工具，不指定路径的话也会自动下载一个。
 os.environ["FFMPEG_BINARY"] = r"D:\Program Files\ffmpeg\bin\ffmpeg.exe"
 
 OUTPUT_DIR = Path("exports")
 FPS = 10
 
-def images_to_video(images: list[np.array], out_file: Path):
+
+def video_to_images(file: Path, sample_fps=None):
+	return list(VideoFileClip(file).iter_frames(sample_fps))
+
+
+def series_to_images(directory: Path):
+	files = os.listdir(directory)
+	images = [None] * len(files)
+
+	# https://github.com/ykuo2/dicom2jpg/blob/main/dicom2jpg/utils.py
+	for file in files:
+		ds = dcmread(directory / file)
+		i, px = ds.InstanceNumber, ds.pixel_array
+
+		px = pixels.apply_modality_lut(px, ds)
+		px = pixels.apply_voi_lut(px, ds)
+		px = pixels.apply_presentation_lut(px, ds)
+
+		min_ = px.min()
+		px = (px - min_) / (px.max() - min_) * 255
+
+		images[i - 1] = px.astype(np.uint8)
+
+	return images
+
+
+def images_to_video(images: list[np.ndarray], out_file: Path):
 	clips = [ImageClip(x, duration=1 / FPS) for x in images]
 	video = concatenate_videoclips(clips, method="compose")
 
@@ -28,31 +55,7 @@ def images_to_video(images: list[np.array], out_file: Path):
 	print("Video created successfully:", out_file)
 
 
-def video_to_images(file: Path, sample_fps=None):
-	return list(VideoFileClip(file).iter_frames(sample_fps))
-
-
-def load_series_images(directory: Path):
-	files = os.listdir(directory)
-	images = [None] * len(files)
-	
-	# https://github.com/ykuo2/dicom2jpg/blob/main/dicom2jpg/utils.py
-	for file in files:
-		ds = dcmread(image_dir / file)
-		i, px = ds.InstanceNumber, ds.pixel_array
-		
-		px = pixels.apply_modality_lut(px, ds)
-		px = pixels.apply_voi_lut(px, ds)
-		px = pixels.apply_presentation_lut(px, ds)
-		
-		px = ((px-px.min())/(px.max()-px.min())) * 255
-		
-		images[i - 1] = px.astype(np.uint8)
-
-	return images
-
-
-def _save_dcms(images: list[np.array], directory: Path):
+def _save_dcms(images: list[np.ndarray], directory: Path):
 	hasher, blobs = sha256(b"Kaciras DICOM Convertor"), []
 
 	for px in images:
@@ -60,21 +63,24 @@ def _save_dcms(images: list[np.array], directory: Path):
 		hasher.update(buffer)
 		blobs.append(buffer)
 
-	hash = str(int.from_bytes(hasher.digest()))
-	prefix = PYDICOM_ROOT_UID + hash[:24].zfill(24)
+	h64 = str(int.from_bytes(hasher.digest()))
+	prefix = PYDICOM_ROOT_UID + h64[:24].zfill(24)
 
 	for i, px in enumerate(images):
 		ds = Dataset()
 		ds.ensure_file_meta()
 
 		ds.SOPClassUID = SecondaryCaptureImageStorage
-		ds.SOPInstanceUID = UID(prefix + str(i))
-	
+		ds.SOPInstanceUID = prefix + str(i)
+		ds.StudyInstanceUID = "1.2.3.4"
+		ds.SeriesInstanceUID = "3.23.53.4666"
+
 		ds.BitsAllocated = 8
 		ds.BitsStored = 8
 		ds.HighBit = ds.BitsStored - 1
 		ds.PixelRepresentation = 0
 		ds.SamplesPerPixel = 1
+		ds.NumberOfFrames = 1
 		ds.Rows = px.shape[0]
 		ds.Columns = px.shape[1]
 		ds.PixelData = blobs[i]
@@ -92,7 +98,7 @@ def _save_dcms(images: list[np.array], directory: Path):
 		ds.save_as(directory / f"{i}.dcm", enforce_file_format=True)
 
 
-def save_images(images: list[np.array], directory: Path, ext = "png"):
+def save_images(images: list[np.ndarray], directory: Path, ext="png"):
 	directory.mkdir(exist_ok=True)
 
 	if ext == "dcm":
@@ -100,14 +106,3 @@ def save_images(images: list[np.array], directory: Path, ext = "png"):
 
 	for i, px in enumerate(images):
 		Image.fromarray(px).save(directory / f"{i}.{ext}")
-
-
-if __name__ == "__main__":
-	image_dir = Path(r"download\test.avi")
-	
-	# images = load_series_images(image_dir)
-	# images_to_video(images, image_dir.with_suffix(".avi"))
-
-	images = video_to_images(image_dir)
-	save_images(images, Path("exports"), "dcm")
-
