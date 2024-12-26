e9fe58fd4d18e4b3f1fdb60eb6ec250ee84c8441
Kaciras <Kaciras@protonmail.com>
2024-12-26 18:23:15 +0800

    refactor format convertor

--- tools/export.py
+++ tools/export.py
@@ -18,91 +18,105 @@ OUTPUT_DIR = Path("exports")
 FPS = 10
 
 
-def video_to_images(file: Path, sample_fps=None):
-	return list(VideoFileClip(file).iter_frames(sample_fps))
+class SeriesImageList:
+	"""
 
+	"""
 
-def series_to_images(directory: Path):
-	files = os.listdir(directory)
-	images = [None] * len(files)
+	name: str
+	images: list[np.ndarray]
 
-	# https://github.com/ykuo2/dicom2jpg/blob/main/dicom2jpg/utils.py
-	for file in files:
-		ds = dcmread(directory / file)
-		i, px = ds.InstanceNumber, ds.pixel_array
+	def __init__(self, name: str, images: list[np.ndarray]):
+		self.name = name
+		self.images = images
 
-		px = pixels.apply_modality_lut(px, ds)
-		px = pixels.apply_voi_lut(px, ds)
-		px = pixels.apply_presentation_lut(px, ds)
+	@staticmethod
+	def from_video(file: Path, sample_fps=None):
+		frames = VideoFileClip(file).iter_frames(sample_fps)
+		return SeriesImageList(file.name, list(frames))
 
-		min_ = px.min()
-		px = (px - min_) / (px.max() - min_) * 255
+	@staticmethod
+	def from_pictures(name: str, files: list[Path]):
+		images = [None] * len(files)
 
-		images[i - 1] = px.astype(np.uint8)
+		for i, file in enumerate(files):
+			image = Image.open(file)
+			images[i] = np.asarray(image)
 
-	return images
+		return SeriesImageList(name, images)
 
+	@staticmethod
+	def from_dcm_files(name: str, files: list[Path]):
+		images = [None] * len(files)
 
-def images_to_video(images: list[np.ndarray], out_file: Path):
-	clips = [ImageClip(x, duration=1 / FPS) for x in images]
-	video = concatenate_videoclips(clips, method="compose")
+		# https://github.com/ykuo2/dicom2jpg/blob/main/dicom2jpg/utils.py
+		for i, file in enumerate(files):
+			ds = dcmread(file)
+			i, px = ds.InstanceNumber, ds.pixel_array
 
-	if out_file.suffix != ".avi":
-		video.write_videofile(out_file, fps=FPS)
-	else:
-		video.write_videofile(out_file, codec="png", fps=FPS)
+			px = pixels.apply_modality_lut(px, ds)
+			px = pixels.apply_voi_lut(px, ds)
+			px = pixels.apply_presentation_lut(px, ds)
 
-	print("Video created successfully:", out_file)
+			min_ = px.min()
+			px = (px - min_) / (px.max() - min_) * 255
 
+			images[i - 1] = px.astype(np.uint8)
 
-def _save_dcms(images: list[np.ndarray], directory: Path):
-	hasher, blobs = sha256(b"Kaciras DICOM Convertor"), []
+		return SeriesImageList(name, images)
 
-	for px in images:
-		buffer = px.tobytes()
-		hasher.update(buffer)
-		blobs.append(buffer)
+	def to_video(self, out_file: Path):
+		clips = [ImageClip(x, duration=1 / FPS) for x in self.images]
+		video = concatenate_videoclips(clips, method="compose")
 
-	h64 = str(int.from_bytes(hasher.digest()))
-	prefix = PYDICOM_ROOT_UID + h64[:24].zfill(24)
-
-	for i, px in enumerate(images):
-		ds = Dataset()
-		ds.ensure_file_meta()
-
-		ds.SOPClassUID = SecondaryCaptureImageStorage
-		ds.SOPInstanceUID = prefix + str(i)
-		ds.StudyInstanceUID = "1.2.3.4"
-		ds.SeriesInstanceUID = "3.23.53.4666"
-
-		ds.BitsAllocated = 8
-		ds.BitsStored = 8
-		ds.HighBit = ds.BitsStored - 1
-		ds.PixelRepresentation = 0
-		ds.SamplesPerPixel = 1
-		ds.NumberOfFrames = 1
-		ds.Rows = px.shape[0]
-		ds.Columns = px.shape[1]
-		ds.PixelData = blobs[i]
-
-		if px.shape[2] == 3:
-			ds.PhotometricInterpretation = "RGB"
-			ds.PlanarConfiguration = 0
+		if out_file.suffix != ".avi":
+			video.write_videofile(out_file, fps=FPS)
 		else:
-			ds.PhotometricInterpretation = "MONOCHROME1"
+			video.write_videofile(out_file, codec="png", fps=FPS)
 
-		ds.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
-		ds.file_meta.MediaStorageSOPInstanceUID = ds.SOPInstanceUID
-		ds.file_meta.MediaStorageSOPClassUID = ds.SOPClassUID
+	def to_pictures(self, directory: Path, ext="png"):
+		directory.mkdir(exist_ok=True)
+		for i, px in enumerate(self.images):
+			Image.fromarray(px).save(directory / f"{i}.{ext}")
 
-		ds.save_as(directory / f"{i}.dcm", enforce_file_format=True)
+	def to_dcm_files(self, directory: Path):
+		hasher, blobs = sha256(b"Kaciras DICOM Convertor"), []
 
+		for px in self.images:
+			buffer = px.tobytes()
+			hasher.update(buffer)
+			blobs.append(buffer)
 
-def save_images(images: list[np.ndarray], directory: Path, ext="png"):
-	directory.mkdir(exist_ok=True)
+		h64 = str(int.from_bytes(hasher.digest()))
+		prefix = PYDICOM_ROOT_UID + h64[:24].zfill(24)
 
-	if ext == "dcm":
-		return _save_dcms(images, directory)
+		for i, px in enumerate(self.images):
+			ds = Dataset()
+			ds.ensure_file_meta()
 
-	for i, px in enumerate(images):
-		Image.fromarray(px).save(directory / f"{i}.{ext}")
+			ds.SOPClassUID = SecondaryCaptureImageStorage
+			ds.SOPInstanceUID = prefix + str(i)
+			ds.StudyInstanceUID = "1.2.3.4"
+			ds.SeriesInstanceUID = "3.23.53.4666"
+
+			ds.BitsAllocated = 8
+			ds.BitsStored = 8
+			ds.HighBit = ds.BitsStored - 1
+			ds.PixelRepresentation = 0
+			ds.SamplesPerPixel = 1
+			ds.NumberOfFrames = 1
+			ds.Rows = px.shape[0]
+			ds.Columns = px.shape[1]
+			ds.PixelData = blobs[i]
+
+			if px.shape[2] == 3:
+				ds.PhotometricInterpretation = "RGB"
+				ds.PlanarConfiguration = 0
+			else:
+				ds.PhotometricInterpretation = "MONOCHROME1"
+
+			ds.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
+			ds.file_meta.MediaStorageSOPInstanceUID = ds.SOPInstanceUID
+			ds.file_meta.MediaStorageSOPClassUID = ds.SOPClassUID
+
+			ds.save_as(directory / f"{i}.dcm", enforce_file_format=True)
